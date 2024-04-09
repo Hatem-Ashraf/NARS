8c32198624e004c3fbb4f221071a8fde5eb36375
sayed zarad <65913794+sayed-zarad@users.noreply.github.com>
2024-04-09 14:43:32 +0200

    test

--- nars-quality-assurance-main/courses/src/routes/marksRoutes.js
+++ nars-quality-assurance-main/courses/src/routes/marksRoutes.js
@@ -3,14 +3,16 @@ const marksController = require("../controllers/marksController");
 const router = express.Router({ mergeParams: true });
 
 // Route for retrieving all marks for a given course
-router.route("/")
+router
+  .route("/")
   .get(marksController.getAllMarks)
   .post(marksController.addAllStudentMarks);
 
 // Route for retrieving marks for a specific student in a course and adding marks for a specific student
-router.route("/:studentId")
+router
+  .route("/:studentId")
   .get(marksController.getStudentMarks)
-  .post(marksController.addStudentMarks)
+  .post(marksController.addAllStudentMarks)
   .patch(marksController.updateStudentMarks);
 
 module.exports = router;
