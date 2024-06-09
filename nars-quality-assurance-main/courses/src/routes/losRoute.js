4663b7169ac11d12c59087e621349ee53f639348
Zeinab A. Younes <66272238+zainabyounes@users.noreply.github.com>
2024-06-09 23:00:40 +0300

    ..

---
+++ nars-quality-assurance-main/courses/src/routes/losRoute.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const losController = require("../controllers/los");
+const router = express.Router({ mergeParams: true });
+
+// Route for retrieving all topics for a given course and adding a new topic to the course
+router.route("/")
+.post(losController.createLos)
+.get(losController.getAllLosByDomain);
+
+router.route("/mulLos")
+.get(losController.getMulLoById)
+.post(losController.createMultipleLos);
+
+router.route("/courses/:courseId").get(losController.getLosByCourseId);
+router.route("/all").get(losController.getAllLos);
+router.route("/:id")
+.patch(losController.updateLos)
+.get(losController.getLosById)
+.delete(losController.deleteLos);
+module.exports = router;
--- nars-quality-assurance-main/courses/src/routes/losRoutes.js
+++ nars-quality-assurance-main/courses/src/routes/losRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const dispLos = require('../controllers/dispLos');
 
-// Route to get LOs by Course ID
-router.get('/los/program/:programId', getLosByProgramId);
+// Route to get LOs by program ID
+router.get('/los/program/:programId', dispLos.getLosByProgramId);
 
 module.exports = router;
