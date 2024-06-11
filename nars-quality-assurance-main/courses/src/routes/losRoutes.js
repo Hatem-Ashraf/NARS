a105d7e0aa2b7ccc6ba2cb15226b08e0fcabe467
Zeinab A. Younes <66272238+zainabyounes@users.noreply.github.com>
2024-06-11 22:19:59 +0300

    ..

--- nars-quality-assurance-main/courses/src/routes/losRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dispLos = require('../controllers/dispLos');
-
-// Route to get LOs by program ID
-router.get('/los/program/:programId', dispLos.getLosByProgramId);
-
-module.exports = router;
