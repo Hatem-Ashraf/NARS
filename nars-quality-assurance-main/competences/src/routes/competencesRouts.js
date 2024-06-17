27011f3fc30a97496db086630b2e12e932ac0d52
mahmoud-dev-bkr <mahmoudabohassibaa@gmail.com>
2024-06-18 02:06:36 +0300

    Added table 6, 7, and 8 in course specs

--- nars-quality-assurance-main/competences/src/routes/competencesRouts.js
+++ nars-quality-assurance-main/competences/src/routes/competencesRouts.js
@@ -4,12 +4,16 @@ const competencesController = require("../controllers/competencesController");
 const router = express.Router();
 
 router.get("/faculty/:facultyId", competencesController.getAll);
+
 router.post("/faculty/:facultyId", competencesController.addCompetences);
-//router.get("/:id", competencesController.getOne);
 
 router.get("/faculty/:facultyId/level/:level", competencesController.getAllByLevel);
-//router.patch("/:id", competencesController.updateOne);
-//router.delete("/:id", competencesController.deleteOne);
+
+
+router.get("/compId/:id", competencesController.getOne)
+    .patch("/compId/:id", competencesController.updateOne)
+    .delete("/compId/:id", competencesController.deleteOne);
+
 
 
 module.exports = router;
